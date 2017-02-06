6ece95e3214f465aee2773555348e48800cd31c1
rupesh-gatlewar <gatlewarrupeshkumar@gmail.com>
2017-02-06 11:18:49 +0530

    First commit

---
+++ src/test/java/com/test/scripts/ClassTwo.java
@@ -0,0 +1,19 @@
+package com.test.scripts;
+
+import org.testng.annotations.Test;
+
+public class ClassTwo {
+	
+	@Test
+	public void metOne()
+	{
+		System.out.println("This is method one");
+	}
+	
+	@Test
+	public void metTwo()
+	{
+		System.out.println("This is method two");
+	}
+
+}
